7c0d9f1b27d489e2481f781a1399caa11eb114a8
LyxHeng <987778734@qq.com>
2017-07-24 15:42:07 +0800

    lyx

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+//
+//	@Bean
+//	public EmbeddedServletContainerFactory servletContainer() {
+//		JettyEmbeddedServletContainerFactory factory =
+//				new JettyEmbeddedServletContainerFactory();
+//		return factory;
+//	}
 }
